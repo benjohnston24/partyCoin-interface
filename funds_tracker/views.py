316987b2c0da3f6054fd81e3fdf069bddca77bfe
Ben Johnston <bjohnston24@gmail.com>
2015-03-10 13:10:06 +1100

    Top 5 party summary working well

--- funds_tracker/views.py
+++ funds_tracker/views.py
@@ -2,14 +2,18 @@
 from django.views import generic
 from funds_tracker.models import Donation
 from django.db.models import Max, Sum
+import operator
 
 # Create your views here.
 MAIN_PARTIES = ['Australian Labor Party (ALP)',
                 'Liberal Party of Australia',
                 'Australian Greens',
                 ]
+#The number of top parties to list in the summary
+NO_TOP_PARTIES = 5
 
 
+#Rename to summaryview
 class IndexView(generic.ListView):
     model = Donation
     template_name = 'funds_tracker/index.html'
@@ -28,5 +32,13 @@ class IndexView(generic.ListView):
         for party in parties:
             totals[party] = max_year_objects.filter(party=party).\
                 aggregate(Sum('amount'))['amount__sum'].__int__()
-        return totals
+        sorted_totals = sorted(totals.items(), key=operator.itemgetter(1),
+                               reverse=True)
+        #Create a blank list to return
+        filtered_parties = []
+        #Return only the most well funded parties
+        for i in range(NO_TOP_PARTIES):
+            filtered_parties.append((sorted_totals[i][0].__str__(),
+                                    sorted_totals[i][1]))
+        return filtered_parties
         #return Donation.objects.order_by('year')
