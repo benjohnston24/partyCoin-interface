cd07fa0b71690cfd5418a31e8b089ab265420774
Ben Johnston <bjohnston24@gmail.com>
2015-04-09 00:41:22 +1000

    State summary working

---
+++ funds_tracker/settings.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+"""!
+-----------------------------------------------------------------------------
+File Name : settings.py
+
+Purpose:
+
+Created: 08-Apr-2015 16:44:22 AEST
+-----------------------------------------------------------------------------
+Revision History
+
+
+
+-----------------------------------------------------------------------------
+S.D.G
+"""
+__author__ = 'Ben Johnston'
+__revision__ = '0.1'
+__date__ = '08-Apr-2015 16:44:22 AEST'
+__license__ = 'MPL v2.0'
+
+## LICENSE DETAILS############################################################
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+##IMPORTS#####################################################################
+
+##############################################################################
+STATES_LIST = ['ACT', 'NSW', 'NT', 'QLD', 'SA', 'TAS', 'WA', 'VIC']
+
+STATES_ABBR = {'NSW': 'New South Wales',
+               'VIC': 'Victoria',
+               'QLD': 'Queensland',
+               'TAS': 'Tasmania',
+               'SA': 'South Australia',
+               'WA': 'Western Australia',
+               'NT': 'Northern Territory',
+               'ACT': 'Australian Capital Territory',
+               }
--- funds_tracker/views.py
+++ funds_tracker/views.py
@@ -32,16 +32,70 @@ from django.shortcuts import render
 from django.views import generic
 from funds_tracker.models import Donation, PartyInfo
 from django.db.models import Max, Sum
-from graphos.renderers import gchart
-from graphos.sources.simple import SimpleDataSource
 import matplotlib.pyplot as plt
 from StringIO import StringIO
 import base64
+from settings import STATES_ABBR, STATES_LIST
 
 import operator
+
+import pdb
 ##############################################################################
 #The number of top parties to list in the summary
 NO_TOP_PARTIES = 4
+LIBERAL_BLUE = '#1e6f88'
+ALP_RED = '#B83537'
+PALMER_YELLOW = '#dfff2c'
+GREENS_GREEN = '#34e01d'
+OTHERS = '#CA6FD6'
+COLOURS = []
+
+
+def get_summary(state):
+    #Get the most recent year
+    max_year = Donation.objects.all().aggregate(Max('year'))['year__max']
+    #Get all objects for that year
+    max_year_objects = Donation.objects.filter(year=max_year.__str__(),
+                                               party_state=state.lower())
+    parties = max_year_objects.values_list('party', flat=True).\
+        order_by('party').distinct()
+    #Get the totals for the most funded parties
+    totals = {}
+    grand_total = 0
+    for party in parties:
+        totals[party] = max_year_objects.filter(party=party).\
+            aggregate(Sum('amount'))['amount__sum'].__int__()
+        grand_total += totals[party]
+    sorted_totals = sorted(totals.items(), key=operator.itemgetter(1),
+                           reverse=True)
+    #Create a blank list to return
+    filtered_parties = []
+    filtered_names = []
+    filtered_values = []
+    if len(totals) < NO_TOP_PARTIES:
+        no_of_parties = len(totals)
+    else:
+        no_of_parties = NO_TOP_PARTIES
+    #Return only the most well funded parties
+    for i in range(no_of_parties):
+        filtered_parties.append((sorted_totals[i][0].__str__(),
+                                sorted_totals[i][1]))
+        filtered_names.append(sorted_totals[i][0].__str__())
+        filtered_values.append(sorted_totals[i][1])
+
+    filtered_names.append('Others')
+    filtered_values.append(grand_total - sum(filtered_values))
+    #Get the logos and wiki pages of the most well funded parties
+    for i in range(len(filtered_parties)):
+        w_l = PartyInfo.objects.filter(party=filtered_parties[i][0]).\
+            values('logo', 'wiki_page')
+        try:
+            filtered_parties[i] += (w_l[0]['logo'], w_l[0]['wiki_page'])
+        except:
+            ##TODO handle this better
+            pass
+
+    return (max_year, filtered_parties, filtered_names, filtered_values)
 
 
 #Rename to summaryview
@@ -51,45 +105,60 @@ class IndexView(generic.ListView):
     context_object_name = 'major_national_parties'
 
     def get_queryset(self):
-        #Get the most recent year
-        max_year = Donation.objects.all().aggregate(Max('year'))['year__max']
-        #Get all objects for that year
-        max_year_objects = Donation.objects.filter(year=max_year.__str__(),
-                                                   party_state='FED')
-        parties = max_year_objects.values_list('party', flat=True).\
-            order_by('party').distinct()
-        #Get the totals for the major national parties
-        totals = {}
-        for party in parties:
-            totals[party] = max_year_objects.filter(party=party).\
-                aggregate(Sum('amount'))['amount__sum'].__int__()
-        sorted_totals = sorted(totals.items(), key=operator.itemgetter(1),
-                               reverse=True)
-        #Create a blank list to return
-        filtered_parties = []
-        #Return only the most well funded parties
-        for i in range(NO_TOP_PARTIES):
-            filtered_parties.append((sorted_totals[i][0].__str__(),
-                                    sorted_totals[i][1]))
+        max_year_FED, filtered_parties_FED, filtered_names, filtered_values = \
+            get_summary('FED')
+        #Colours for pie chart
+        COLOURS = []
+        ##TODO add database of colour codes for parties
+        for name in filtered_names:
+            if name.lower().find('liberal') >= 0:
+                COLOURS.append(LIBERAL_BLUE)
+            elif name.lower().find('labor') >= 0:
+                COLOURS.append(ALP_RED)
+            elif name.lower().find('palmer') >= 0:
+                COLOURS.append(PALMER_YELLOW)
+            elif name.lower().find('greens') >= 0:
+                COLOURS.append(GREENS_GREEN)
+            else:
+                COLOURS.append(OTHERS)
 
-        #Get the logos and wiki pages of the most well funded parties
-        for i in range(len(filtered_parties)):
-            w_l = PartyInfo.objects.filter(party=filtered_parties[i][0]).\
-                values('logo', 'wiki_page')
-            filtered_parties[i] += (w_l[0]['logo'], w_l[0]['wiki_page'])
-        #print filtered_parties
-
-        #Temp
-        x = [1, 2, 3, 4, 5]
-        y = [1, 2, 3, 4, 5]
-        fig = plt.plot(x, y)
+        #Generate Pie chart of national donations
+        patches, texts, autotexts = plt.pie(filtered_values,
+                                            labels=filtered_names,
+                                            autopct='%1.1f%%',
+                                            radius=0.8,
+                                            wedgeprops={'edgecolor': 'white',
+                                                        'linewidth': 2},
+                                            colors=COLOURS)
+        for t in texts:
+            t.set_size('smaller')
+            t.set_family('Arial')
+        for t in autotexts:
+            t.set_size('smaller')
+            t.set_color('white')
+            t.set_weight('bold')
         chart_buffer = StringIO()
-        plt.savefig(chart_buffer, format="png")
+        plt.savefig(chart_buffer, bbox_inches='tight', format="png")
         chart = base64.b64encode(chart_buffer.getvalue())
 
-        return {'summary': filtered_parties,
-                'chart': chart}
-        #return Donation.objects.order_by('year')
+        #Create a dictionary for statewide summaries
+        state_names = []
+        state_values = {}
+
+        #Get the NSW results
+        for state in STATES_LIST:
+            max_year, filtered_parties, filtered_names, filtered_values = \
+                get_summary(state)
+            state_names.append(STATES_ABBR[state])
+            state_values[STATES_ABBR[state]] = filtered_parties
+
+        print state_values['New South Wales']
+        return {'year': '%s - %d' % (max_year_FED, int(max_year_FED) + 1),
+                'summary': filtered_parties_FED,
+                'state_names': state_names,
+                'state_values': state_values,
+                'chart': chart,
+                }
 
 
 class ImageView(generic.ListView):
